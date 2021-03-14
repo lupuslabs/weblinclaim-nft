c47a62fe2671f62aa715ad4aaf6fce1bf4347c6a
Suat Özgür <s.oezguer@gmail.com>
2021-03-14 11:52:50 +0100

    speedup sync script

---
+++ scripts/speedupTx.js
@@ -0,0 +1,43 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const network = process.argv[2] ?? 'rinkeby';
+const config = require('./config.' + network + '.js');
+const web3 = require("web3");
+var readlineSync = require('readline-sync');
+
+if (config.mnemonic == null) {
+    config.mnemonic = readlineSync.question('May I have your mnemonic? ');
+}
+let provider;
+try {
+    provider = new HDWalletProvider(config.mnemonic, config.providerUrl);
+} catch (e) {
+    console.log(e.message);
+    process.exit();
+}
+const web3Instance = new web3(provider);
+const nftContract = new web3Instance.eth.Contract(
+    config.abi,
+    config.contractAddress,
+    {gasLimit: "1000000"}
+);
+
+(async () => {
+
+    let account = config.ownerAddress; //'0xBFe63Cc83FE538DCa77c613A4dFAd126a5b052Df';
+    let transactionCount = await web3Instance.eth.getTransactionCount(account);
+    let transactionCountIncludingPending = await web3Instance.eth.getTransactionCount(account, "pending");
+    console.log(transactionCount, transactionCountIncludingPending);
+
+
+    web3Instance.eth.sendTransaction({
+        from: config.ownerAddress,
+        gasPrice: 200e9,
+        gas: "21000",
+        to: config.ownerAddress,
+        value: "0",
+        data: "",
+        nonce: 0,
+    }, '').then(console.log);
+
+//    process.exit();
+})();
