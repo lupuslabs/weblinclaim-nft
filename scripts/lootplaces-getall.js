4ec941d30d46b1eb0da0b336e90e0997065d0b26
Heiner Wolf <git@heinerwolf.de>
2021-09-14 21:23:48 +0200

    check data

---
+++ scripts/lootplaces-getall.js
@@ -0,0 +1,48 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+const web3 = require('web3');
+const { config, contract, abi } = require('./LootPlacesContractWalletProvider.js');
+
+let startIdx = 354;
+let countIdx = 173;
+
+(async () =>
+{
+    const provider = new HDWalletProvider(config.mnemonic, config.providerUrl);
+    const web3Instance = new web3(provider);
+    const nftContract = new web3Instance.eth.Contract(
+        abi,
+        config.contractAddress
+    );
+
+    for (let i = 0; i < countIdx; i++) {
+
+        let idx = startIdx + i;
+        //console.log(`tokenByIndex ${idx}`);
+
+        try {
+            let id = await new Promise((resolve, reject) =>
+                nftContract.methods
+                    .tokenByIndex(idx)
+                    .call()
+                    .then((result) => { resolve(result); })
+                    .catch((err) => { reject(err); })
+            )
+            //console.log(`tokenByIndex ${idx} => ${id}`);
+
+            let data = await new Promise((resolve, reject) =>
+                nftContract.methods
+                    .getDomain(id)
+                    .call()
+                    .then((result) => { resolve(result); })
+                    .catch((err) => { reject(err); })
+            )
+            console.log(`${idx}\t${id}\t${data}`);
+
+        } catch (err) {
+            console.log(`Catch`, err);
+        }
+
+        //        await new Promise(resolve => setTimeout(resolve, 1000));
+    }
+
+})();
