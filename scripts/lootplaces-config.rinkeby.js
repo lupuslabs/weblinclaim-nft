e165c874478a259f9c5f5d6d3b8674ed39827b1e
Heiner Wolf <git@heinerwolf.de>
2021-09-15 10:28:37 +0200

    curent test contract

--- scripts/lootplaces-config.rinkeby.js
+++ scripts/lootplaces-config.rinkeby.js
@@ -1,5 +1,5 @@
 module.exports = {
-    contractAddress: '0x53d70283B38A89e25cD6E9AD5f25C5dEea56bFC8',
+    contractAddress: '0xf83E58259607E29E0c69d199C856D9900EAEA885',
     ownerAddress: '0x2410d968fbbe5a7aad975B5244Ea9812C5e5C141',
     providerUrl: 'https://eth-rinkeby.alchemyapi.io/v2/r2gUsunv9dqoULzKRpZsIwo2MgOIYkO9',
     mnemonic: 'cannon weird charge face awful grow already mountain syrup paper sport robot',
